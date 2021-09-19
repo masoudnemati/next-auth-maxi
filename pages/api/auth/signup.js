b8396c300bc8ff3c5aabf83604317f6568d7fc99
masoudnemati <masoud.nnr@gmail.com>
2021-09-19 17:20:34 +0430

    everything works till part 6/20

---
+++ pages/api/auth/signup.js
@@ -0,0 +1,33 @@
+import { hashPassword } from "../../../lib/auth";
+import { connectToDatabase } from "../../../lib/db";
+
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+    const { email, password } = data;
+
+    if (
+      !email ||
+      !email.includes("@") ||
+      !password ||
+      password.trim().length < 7
+    ) {
+      res.status(422).json({ message: "invalid input" });
+      return;
+    }
+
+    const client = await connectToDatabase();
+    const db = client.db();
+
+    const hashedPassword = await hashPassword(password);
+
+    const result = await db.collection("users").insertOne({
+      email: email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({ message: "Created User." });
+  }
+}
+
+export default handler;
