10ab2cf24116205daf76862b961e6eb16e6df588
masoudnemati <masoud.nnr@gmail.com>
2021-09-21 17:18:39 +0430

    built till part 18, needs to build change password, everything works fine

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -19,6 +19,13 @@ async function handler(req, res) {
     const client = await connectToDatabase();
     const db = client.db();
 
+    const existingUser = await db.collection("users").findOne({ email: email });
+    if (existingUser) {
+      res.status(422).json({ message: "User exists already!" });
+      client.close();
+      return;
+    }
+
     const hashedPassword = await hashPassword(password);
 
     const result = await db.collection("users").insertOne({
@@ -27,6 +34,7 @@ async function handler(req, res) {
     });
 
     res.status(201).json({ message: "Created User." });
+    client.close();
   }
 }
 
